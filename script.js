33adee8c45d88911a72970b3b9de87e0ced55115
Mohan <102119043@nitt.edu>
2020-09-30 20:53:39 +0530

    keycode is deprecated and Infinity error

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-var input = document.getElementById("display");
+var input = document.getElementById('display');
 
 //script to detect if enter button is clicked to evaluate the result
+const calc = () => {
+  if (eval(display.value) === Infinity) {
+    input.value = 'Error';
+  } else {
+    input.value = eval(display.value);
+  }
+};
 
-input.addEventListener("keyup", function(event) {
-  
-    // Number 13 is the "Enter" key on the keyboard
-  
-    if (event.keyCode == 13) {
-    
-      // Trigger the button element with a click
-      
-      document.getElementById("equal").click();
+input.addEventListener('keyup', function (event) {
+  // Number 13 is the "Enter" key on the keyboard
+
+  if (event.key == 'Enter' || event.keyCode == '13') {
+    // Trigger the button element with a click
+    calc();
   }
 });
