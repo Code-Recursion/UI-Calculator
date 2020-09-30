0b837921618dc19b2393c1d373c951bfcd6729f0
Sanket vyawahare <sanketvy7@gmail.com>
2020-09-30 23:55:08 +0530

    added decimal operation, handled divideby0 error

--- script.js
+++ script.js
@@ -3,13 +3,23 @@ var input = document.getElementById("display");
 //script to detect if enter button is clicked to evaluate the result
 
 input.addEventListener("keyup", function(event) {
-  
     // Number 13 is the "Enter" key on the keyboard
-  
     if (event.keyCode == 13) {
-    
       // Trigger the button element with a click
-      
       document.getElementById("equal").click();
   }
 });
+
+function calculate(){
+  let data = document.getElementById('display').value;
+
+  if (data.substr(-2)=="/0" || data.substr(-3)=="/00"){
+    document.getElementById('display').value = "Invalid";
+    setTimeout(()=>{
+      document.getElementById('display').value = "";
+      console.error("divide by 0 not allowed");
+    },500);
+  }else{
+    document.getElementById('display').value = eval(data);
+  }
+}
